f4be49d1ab585ea9bc786416f0502d73c1374199
frosty <passedgoandgot200@gmail.com>
2024-05-26 11:05:31 -0400

    add qutebrowser

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.load_autoconfig(False)
+
+c.content.geolocation = False
+
+config.bind('xb', 'config-cycle statusbar.show always never')
+config.bind('xt', 'config-cycle tabs.show always never')
+config.bind('xx', 'config-cycle tabs.show always never ;; config-cycle statusbar.show always never')
