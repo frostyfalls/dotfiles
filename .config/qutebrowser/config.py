06ac867b18f4c2ab6f40e5dceb406c8faf42294b
frosty <passedgoandgot200@gmail.com>
2024-07-03 20:06:49 -0400

    qutebrowser font settings

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,3 +3,6 @@ config.load_autoconfig(False)
 config.bind('xb', 'config-cycle statusbar.show always never')
 config.bind('xt', 'config-cycle tabs.show always never')
 config.bind('xx', 'config-cycle tabs.show always never ;; config-cycle statusbar.show always never')
+
+c.fonts.default_family = 'Terminus'
+c.fonts.default_size = '12px'
