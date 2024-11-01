7e7f5d856863341d5075f3eaec32eb265044282b
frosty <passedgoandgot200@disroot.org>
2024-10-31 22:54:20 -0400

    feat(nvim): add git-blame.nvim

--- .config/nvim/lua/frosty-plugins/frosty-git.lua
+++ .config/nvim/lua/frosty-plugins/frosty-git.lua
@@ -9,4 +9,13 @@ return {
       vim.keymap.set('n', '<leader>g', neogit.open)
     end,
   },
+  {
+    'f-person/git-blame.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.g.gitblame_delay = 1000
+      vim.g.gitblame_ignored_filetypes = { 'netrw' }
+      vim.g.gitblame_highlight_group = 'CursorLine'
+    end,
+  },
 }
