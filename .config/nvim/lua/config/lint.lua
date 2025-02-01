634c82d94a4f89fbc163cadc1b6df31a80c3d392
frosty <passedgoandgot200@disroot.org>
2025-01-31 22:48:39 -0500

    nvim: remove unneeded files

--- .config/nvim/lua/config/lint.lua
+++
@@ -1,9 +0,0 @@
-require('lint').linters_by_ft = {
-  sh = { 'shellcheck' },
-}
-
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-  callback = function()
-    require('lint').try_lint()
-  end,
-})
