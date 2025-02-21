1c504e5f3c8284e9e6f05cc938a33c1ceb595115
frosty <passedgoandgot200@disroot.org>
2025-02-21 09:38:28 -0500

    nvim: cleanup and remove old config

--- pkg/nvim/.config/nvim/init.lua
+++ pkg/nvim/.config/nvim/init.lua
@@ -31,8 +31,6 @@ require('paq')({
   'hrsh7th/cmp-path',
   'hrsh7th/cmp-cmdline',
   'hrsh7th/nvim-cmp',
-  -- 'L3MON4D3/LuaSnip',
-  -- 'saadparwaiz1/cmp_luasnip',
 })
 
 vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
@@ -90,11 +88,6 @@ vim.api.nvim_set_keymap('n', '<Leader>s', ':setlocal spell!<CR>', { noremap = tr
 local cmp = require('cmp')
 
 cmp.setup({
-  -- snippet = {
-  --   expand = function(args)
-  --     require('luasnip').lsp_expand(args.body)
-  --   end,
-  -- },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -130,7 +123,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local servers = {
   clangd = {},
   gopls = {},
-  rust_analyzer = {},
   lua_ls = {
     Lua = {
       diagnostics = {
