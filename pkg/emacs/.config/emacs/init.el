262f84729ae20ab23bb2d101ca3061094df83c04
frosty <passedgoandgot200@disroot.org>
2025-03-13 12:52:49 -0400

    emacs: update

--- pkg/emacs/.config/emacs/init.el
+++ pkg/emacs/.config/emacs/init.el
@@ -1,13 +1,17 @@
-;;; init.el --- Personal configuration  -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :noerror)
+
+;;; Package Manager
 
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (unless package-archive-contents
   (package-refresh-contents))
+
+;;; Interface
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -15,18 +19,54 @@
 (setq inhibit-splash-screen t)
 
 (set-face-attribute 'default nil
-                    :font "Liberation Mono"
+                    :font "DejaVu Sans Mono"
                     :height 90
                     :weight 'regular)
 
-(use-package display-line-numbers
-  :hook (prog-mode emacs-lisp-mode)
-  :custom
-  (display-line-numbers-type 'relative))
+(set-face-attribute 'font-lock-comment-face nil
+		    :slant 'italic)
 
 (use-package gruber-darker-theme
   :ensure t
   :init (load-theme 'gruber-darker t))
+
+(use-package display-line-numbers
+  :hook (prog-mode emacs-lisp-mode)
+  :custom
+  (display-line-numbers-type 'relative)
+  (display-line-numbers-width 3))
+
+(setq use-short-answers t)
+(setq use-dialog-box nil)
+
+;;; Editor Behavior
+
+(electric-pair-mode t)
+(setq kill-whole-line t)
+
+(global-auto-revert-mode t)
+(savehist-mode t)
+(save-place-mode t)
+
+(add-hook 'before-save-hook #'delete-trailing-whitespace)
+
+;;; Global Keymaps
+
+(keymap-global-set "C-;" "C-a C-SPC C-n M-w C-y C-p")
+
+;;; Org
+;; TODO(frosty): Set up better binds for jumping around Org documents
+
+;; (use-package org
+;;   :hook (org-mode . org-indent-mode)
+;;   :custom
+;;   (org-directory "~/documents/org/")
+;;   (org-journal-dir (expand-file-name org-directory "journal/"))
+;;   (org-agenda-files '((expand-file-name org-directory "agenda.org")))
+;;   (org-default-notes-file (expand-file-name org-directory "notes.org")))
+
+;;; Minibuffer Completion
+;; TODO(frosty): Set up better binds
 
 (use-package vertico
   :ensure t
@@ -36,16 +76,40 @@
 	      ("DEL" . vertico-directory-delete-word)
 	      ("M-d" . vertico-directory-delete-char)))
 
+(use-package marginalia
+  :ensure t
+  :init (marginalia-mode t))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;;; Completion at Point
+;; TODO(frosty): Set up snippets and spell-checking
+
 (use-package corfu
   :ensure t
-  :hook (prog-mode emacs-lisp-mode))
+  :hook (prog-mode emacs-lisp-mode)
+  :custom
+  (corfu-quit-no-match 'separator))
+
+(setq tab-always-indent 'complete)
+
+;;; Git
+;; TODO(frosty): Set up inline diff information
 
 (use-package magit
   :ensure t
-  :custom
-  (magit-diff-refine-hunk t)
   :bind (:map global-map
-	      ("C-c g" . magit-status)))
+	      ("C-c g" . magit-status))
+  :custom
+  (magit-diff-refine-hunk t))
+
+
+;;; which-key
+;; TODO(frosty): Tweak options
 
 (use-package which-key
   :ensure t
@@ -53,30 +117,24 @@
   :custom
   (which-key-side-window-location 'bottom))
 
-(use-package flymake
-  :hook (emacs-lisp-mode sh-mode)
-  :bind (:map flymake-mode-map
-	      ("C-c n" . flymake-goto-next-error)
-	      ("C-c p" . flymake-goto-prev-error)))
+;;; Syntax Checking
+;; TODO(frosty): Tweak options
+;; TODO(frosty): Use each major mode's package for initialization
+
+(use-package flycheck
+  :ensure t
+  :hook (sh-mode c-mode go-mode))
+
+;;; Language Servers
+
+(use-package flycheck-eglot
+  :ensure t
+  :after (flycheck eglot)
+  :init (global-flycheck-eglot-mode t))
 
 (use-package eglot
-  :hook (c-mode go-mode . 'eglot-ensure))
+  :hook ((c-mode go-mode) . eglot-ensure))
 
-(setq window-resize-pixelwise t)
-(save-place-mode t)
-(savehist-mode t)
-(defalias 'yes-or-no #'y-or-n-p)
-
-(setq kill-whole-line t)
-(global-set-key [escape] 'keyboard-escape-quit)
-(keymap-global-set "C-;" "C-a C-SPC C-n M-w C-y C-p")
-(electric-pair-mode t)
-
-(use-package go-mode
-  :ensure t)
-
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-;;; init.el ends here
+(use-package editorconfig
+  :ensure t
+  :init (editorconfig-mode t))
